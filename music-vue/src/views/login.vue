42c29068a79f50312fcfbe522fc735f44e125f45
tianjian <969530555@qq.com>
2019-09-03 17:30:02 +0800

    vue3.0

---
+++ music-vue/src/views/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  components: {
+
+  },
+  created(){
+    console.log(this)
+  }
+}
+</script>
